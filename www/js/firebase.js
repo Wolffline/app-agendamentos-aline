b7c8c3b76490cd0f0bc9e2489f13f3a67c693ea6
Wolffline <alineesilva101@gmail.com>
2021-09-22 23:29:08 -0300

    first commit

---
+++ www/js/firebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyB2HZdda3HmMuLmKcZnsx3JCfBDb8BewgQ",
+  authDomain: "appagendamentos.firebaseapp.com",
+  projectId: "appagendamentos",
+  storageBucket: "appagendamentos.appspot.com",
+  messagingSenderId: "969829882134",
+  appId: "1:969829882134:web:53ad8996a4bbe3a647d837",
+  measurementId: "G-T12D88CFTY"
+  };
+  
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+//firebase.getAnalytics(app);
